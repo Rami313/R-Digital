55dfec9575ab8bd813c8809da4ce43004234dc65
Rami Ad <rami.ko313@gmail.com>
2024-01-03 22:18:27 +0200

    fisrt commit
    Full-Stack E-Commerce

---
+++ libs/firebase.ts
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// my web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC9hcHWXoeq43zTZL74W7wNMJq3D4Vu4fU",
+  authDomain: "r-digital-410117.firebaseapp.com",
+  projectId: "r-digital-410117",
+  storageBucket: "r-digital-410117.appspot.com",
+  messagingSenderId: "1010664415727",
+  appId: "1:1010664415727:web:088c53e0cca1f1fd96d61a",
+};
+// Initialize Firebase
+const firebaseApp = initializeApp(firebaseConfig);
+
+export default firebaseApp;
